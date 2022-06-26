abf38cd883750b07ab2f8172d2fb6ea9d425d72f
PPS <pavlina.psarsky@gmail.com>
2022-06-26 18:17:58 +0200

    mobile layout done

--- js/app.js
+++ js/app.js
@@ -1,47 +1,31 @@
 //https://www.quotes.net/quotes_api.php
 
-//let background = document.getElementById('container');
-
-async function loadQuotesData() {
-    const response = await fetch("https://type.fit/api/quotes");
-    const data = await response.json();
-    return data;
-
-    // try {
-    //     const response = await fetch("https://type.fit/api/quotes");
-    //     console.log(response)
-    //     if(!response.ok) throw Error('Did not receive expected data');
-    //     const data = await response.json();
-    //     quoteData = data;
-    //     console.log(quoteData)
-    //     return quoteData;
-        //console.log(quoteData);
-    //     setItems(listItems);
-    //     setFetchError(null);
-      // } catch (err) {
-     //  console.log(err.message);
-    //   } finally {
-    //     setIsLoading(false)
-    //   }
-//}
-}
-
 let i = 0;
 function getRandomBg() {
     $("#container").css('background-image', `url("https://source.unsplash.com/random/700x500?space,ocean=${i}")`);
     i++;
 }
 
+async function loadQuotesData() {
+    const response = await fetch("https://type.fit/api/quotes");
+    if(!response.ok) throw Error('Did not receive expected data');
+    const data = await response.json();
+    return data;
+}
+
 async function getQuote() {
     let quotes = {};
     try {
         quotes = await loadQuotesData();
-        console.log(quotes)
         let randomIndex = Math.floor(Math.random() * quotes.length);
         $("#text").text(quotes[randomIndex].text);
         $("#author").text(quotes[randomIndex].author);
-    } catch {
-        console.log('Error!')
+        $('#tweet-quote').attr(
+            'href',
+            `https://twitter.com/intent/tweet?text="${quotes[randomIndex].text}" By ${quotes[randomIndex].author}`
+          );
+    } catch (error) {
+        console.log(err.message);
     }
  }
     
